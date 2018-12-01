a6ab30955b599cade05b979f910d90747c8925bf
ThiagoCoutinho <thiago18coutinho@gmail.com>
2018-12-01 13:06:03 -0200

    "Classification"

---
+++ Templates/2D Data Charts.py
@@ -0,0 +1,28 @@
+## Regression
+plt.scatter(X_treino, y_treino, c="red",)
+plt.plot(X_treino, regressor.predict(X_treino), c="blue")
+plt.title('Salario vs Anos de Experiência')
+plt.xlabel('Anos de Experiência')
+plt.ylabel('Salário')
+
+## Classification
+# Visualising the Training set results
+
+# If we want to visualize Test set just need to change in X_set, y_set = X_test, y_test
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
